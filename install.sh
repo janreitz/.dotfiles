3d149080b602492a64aa6ec2d32157d7ae6689b0
Jan Reitz <jan-reitz@gmx.de>
2020-09-01 19:51:27 +0200

    add .zshrc and zsh setup to install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
-# Symbolic links
+apt install zsh
+apt-get install fonts-powerline
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ 
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+# spaceship-prompt symlink
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" 
+
+# Substitute existing dotfiles with symbolic links
 dotfiles=(
 	".bashrc"
 	".bash_aliases"
 	".vimrc"
 	".env"
+	".zshrc"
 )
 
 for dotfile in "${dotfiles[@]}"; do
-	rm "${HOME}/$dotfile"
-	
+	rm -rf "${HOME}/$dotfile"
 	ln -s "$(pwd)/$dotfile" "${HOME}/$dotfile"
 done
+
  
