dc1e0d3628d7d552fe2b04bda76827e488fe582e
Jan Reitz <jan-reitz@gmx.de>
2021-05-04 13:55:13 +0000

    do not install any programs, just link the dotfiles

--- install.sh
+++ install.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-apt install zsh
-apt-get install fonts-powerline
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
- 
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-# spaceship-prompt symlink
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" 
-
 # Backup exiting dotfiles and add symblic links to the ones in this dir
 OLD_DOTFILES="${HOME}/.dotfile_backup_$(date -u +"%Y%m%d%H%M%S")"
 mkdir $OLD_DOTFILES
@@ -33,10 +24,10 @@ for dotfile in "${dotfiles[@]}"; do
 	ln -s "$(pwd)/$dotfile" "${HOME}/$dotfile"
 done
 
-PROGRAMS=( git vim zsh broot )
-for program in ${PROGRAMS[@]}"; do
-	stow -v --target=$HOME $program
-	echo "Configuring $program"
-done
+# PROGRAMS=( git vim zsh broot )
+# for program in ${PROGRAMS[@]}"; do
+# 	stow -v --target=$HOME $program
+# 	echo "Configuring $program"
+# done
 
 echo "Done!"
